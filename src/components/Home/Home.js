9b2292f5d7ed094aab15388231a7a5967d835b0b
julilan <juli.molnar@outlook.com>
2023-05-16 01:16:09 +0300

    update home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,35 @@
 import React from "react";
-import classes from "./Home.module.css";
+import Banner from "../Banner/Banner";
+import LandingCard from "../UI/LandingCard/LandingCard";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Here will be landing page</div>;
+  return (
+    <div className="home">
+      <Banner />
+      <h2>Looking for recipes?</h2>
+      <div className="card_wrapper">
+        <LandingCard
+          heading="Browse recipes"
+          info="Find your favourites in this collection. You can search recipes based on or country"
+          link="/recipes"
+          linktext="All recipes"
+        />
+        <LandingCard
+          heading="Add recipes"
+          info="Recipe from your country is missing? No worries, add one!"
+          link="/add-new"
+          linktext="Add recipes"
+        />
+        <LandingCard
+          heading="Want to know more about our projects?"
+          info="Visit our programme homepage"
+          link="https://www.bc.fi/koulutukset/koodaajakoulutus-tieto-ja-viestintatekniikan-perustutkinnon-osat/"
+          linktext="Business College Helsinki homepage"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;

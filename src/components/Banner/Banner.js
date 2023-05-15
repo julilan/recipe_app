8dbca7124a1fe0d15a808c8972f7bd86bc81b2d3
julilan <juli.molnar@outlook.com>
2023-05-16 01:09:39 +0300

    update banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,18 @@
 import React from "react";
 import classes from "./Banner.module.css";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className={classes.banner}>
+      <h4>Realm of Recipes</h4>
+      <p>
+        Recipe app which is a final task for React basics course for REACT23K
+        group
+      </p>
+      <Link to="/recipes">Browse recipes</Link>
+    </div>
+  );
 };
 
 export default Banner;

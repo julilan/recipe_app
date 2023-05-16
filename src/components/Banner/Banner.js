50cc82c344829972152a1c746a0ede36dbeadea4
julilan <juli.molnar@outlook.com>
2023-05-16 12:49:11 +0300

    add bg video

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./Banner.module.css";
 import { Link } from "react-router-dom";
+import video from "../../assets/video.mp4";
 
 const Banner = () => {
   return (
     <div className={classes.banner}>
+      <video id="bg-video" src={video} autoPlay loop muted />
       <h4>Realm of Recipes</h4>
       <p>
         Recipe app which is a final task for React basics course for REACT23K
